8bf1bde15a005ca645428b66350c65250896d172
Menyhert <otm@bridgekosice.sk>
2022-12-08 13:02:28 +0100

    Naprogramovaná väčšina funkcií pásika

--- main.ts
+++ main.ts
@@ -9,6 +9,22 @@ while (true) {
     LedPasik.showColor(neopixel.rgb(0, 0, 5))
     basic.pause(500)
     LedPasik.showColor(neopixel.rgb(5, 5, 5))
+    basic.pause(2000)
+    LedPasik.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    LedPasik.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+    LedPasik.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+    LedPasik.setPixelColor(3, neopixel.colors(NeoPixelColors.Yellow))
+    LedPasik.setPixelColor(4, neopixel.colors(NeoPixelColors.Orange))
+    LedPasik.setPixelColor(5, neopixel.colors(NeoPixelColors.Indigo))
+    LedPasik.setPixelColor(6, neopixel.colors(NeoPixelColors.Violet))
+    LedPasik.setPixelColor(7, neopixel.colors(NeoPixelColors.Purple))
+    LedPasik.show()
+    basic.pause(2000)
+    for (let index = 0; index < 100; index++) {
+        LedPasik.rotate(1)
+        LedPasik.show()
+        basic.pause(100)
+    }
 }
 basic.forever(function () {
 	
